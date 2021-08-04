f1572d5cb66983d3101d5f543885009d646d6007
alfiee47 <alfredagyeimarfo@gmail.com>
2021-08-03 23:24:45 -0400

    added authentication

--- NodeJS/conFusionServer/routes/leaderRouter.js
+++ NodeJS/conFusionServer/routes/leaderRouter.js
@@ -1,62 +1,80 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const leaderRouter = express.Router();
+const mongoose = require('mongoose');
+
+const Leaders = require('../models/leaders');
 
 leaderRouter.use(bodyParser.json());
 leaderRouter.route('/')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader = ('Content-Type', 'text/plain')
-        next();
-    })
 
 .get((req, res, next) => {
-    res.end('will send all the leader to you!');
-})
-
-.post((req, res, next) => {
-    res.end('will add the leader:' + req.body.name + 'with details:' + req.body.description);
-
-})
-
-.put((req, res, next) => {
-    res.statusCode = 403;
-    res.end('put operation not supported');
-
-})
-
-
-.delete((req, res, next) => {
-    res.end('Deleting all leaders');
-});
-
-
-leaderRouter.route("/:leaderId")
-    .get((req, res, next) => {
-        res.end('will send you   ' + req.params.leaderId);
+        Leaders.find({})
+            .then((leaders) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leaders);
+            }, (err) => next(err))
+            .catch((err) => next(err));
     })
+    .post((req, res, next) => {
+        Leaders.create(req.body)
+            .then((leader) => {
+                console.log('leader Created ', leader);
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leader);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .put((req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /Leaders');
+    })
+    .delete((req, res, next) => {
+        Leaders.remove({})
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
 
-.post((req, res, next) => {
-    res.end('will add the leader:' + req.body.name + 'with details:' + req.body.description);
-    res.statusCode = 403;
-    res.end('POST operation not supported on /leaderss/' + req.params.leaderId);
-
-})
-
-.put((req, res, next) => {
-    res.write('Updating the leader: ' + req.params.leaderId + '\n');
-    res.end('Will update the leader: ' + req.body.name +
-        ' with details: ' + req.body.description);
-
-})
-
-
-.delete((req, res, next) => {
-    res.end('Deleting leader: ' + req.params.leaderId);
-});
-
-
-
+leaderRouter.route('/:leaderId')
+    .get((req, res, next) => {
+        Leaders.findById(req.params.leaderId)
+            .then((leader) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leader);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /Leaders/' + req.params.leaderId);
+    })
+    .put((req, res, next) => {
+        Leaders.findByIdAndUpdate(req.params.leaderId, {
+                $set: req.body
+            }, { new: true })
+            .then((leader) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(leader);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .delete((req, res, next) => {
+        Leaders.findByIdAndRemove(req.params.leaderId)
+            .then((resp) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(resp);
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    });
 
 
 
